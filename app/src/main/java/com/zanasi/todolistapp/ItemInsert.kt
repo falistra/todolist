2fae361ecca28fc0c225016effab65dae2dffce7
Fausto Zanasi <fausto.zanasi@gmail.com>
2022-06-05 09:59:13 +0200

    add database

--- app/src/main/java/com/zanasi/todolistapp/ItemInsert.kt
+++ app/src/main/java/com/zanasi/todolistapp/ItemInsert.kt
@@ -2,10 +2,39 @@ package com.zanasi.todolistapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ItemInsert : AppCompatActivity() {
+
+    private lateinit  var database: DatabaseReference
+    var toDOList : MutableList<ToDoModel>? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_insert)
+
+        database = FirebaseDatabase.getInstance().reference
+
+        val bottoneAdd: Button = findViewById(R.id.addItemConfirm)
+        bottoneAdd.setOnClickListener {
+            val todoItemData = ToDoModel.createList()
+            val what : EditText = findViewById(R.id.editText_what)
+            todoItemData.itemDataText = what.text.toString()
+            val newItemData = database.child("todo").push()
+            todoItemData.UID = newItemData.key
+            newItemData.setValue(todoItemData)
+
+            onBackPressed()
+        }
     }
+
+/*
+    bottoneAdd.setOnClickListener {
+        finishActivity(0)
+    }
+*/
 }
